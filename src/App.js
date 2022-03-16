4017876d3486b1002ff6091968f0f4704bde788b
Autobotbrain <98964236+Autobotbrain@users.noreply.github.com>
2022-03-16 19:25:42 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,228 +1,209 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import axios from 'axios';
 import './App.css';
 
-
 class App extends Component {
-  constructor() {
+  constructor(){
     super()
-    this.state = {
-      firstName:'',
-        lastName:'',
-        email:'',
-        password:'',
-        confirmpassword:'',
-        nic:'',
-        address:'',
-        contactNumbers:'',
-        childName:'',
-        childSchool:'',
-      
-        
+    this.state ={
+        FirstName:'',
+        LastName:'',
+        Email:'',
+        Password:'',
+        ConfirmPassword:'',
+        Phone_Number:'',
+        NIC_Number:'',
+        Driving_Licence_Id:'',
+        Address:''
     }
 
-    this.changefirstName=this.changefirstName.bind(this)
-    this.changelastName=this.changelastName.bind(this)
-    this.changeemail=this.changeemail.bind(this)
-    this.changepassword=this.changepassword.bind(this)
-    this.changeconfirmpassword=this.changeconfirmpassword.bind(this)
-    this.changenic=this.changenic.bind(this)
-    this.changeaddress=this.changeaddress.bind(this)
-    this.changecontactNumbers=this.changecontactNumbers.bind(this)
-    this.changechildName=this.changechildName.bind(this)
-    this.changechildSchool=this.changechildSchool.bind(this)
-    this.onSubmit=this.onSubmit.bind(this)
+    this.changeFirstName=this.changeFirstName.bind(this);
+    this.changeLastName=this.changeLastName.bind(this);
+    this.changeEmail=this.changeEmail.bind(this);
+    this.changePassword=this.changePassword.bind(this);
+    this.changeConfirmPassword=this.changeConfirmPassword.bind(this);
+    this.changePhone_Number=this.changePhone_Number.bind(this);
+    this.changeNIC_Number=this.changeNIC_Number.bind(this);
+    this.changeDriving_Licence_Id=this.changeDriving_Licence_Id.bind(this);
+    this.changeAddress=this.changeAddress.bind(this);
+    this.onSubmit=this.onSubmit.bind(this);
 
   }
 
-  changefirstName(event) {
+  changeFirstName(event){
     this.setState({
-      firstName:event.target.value
+      FirstName:event.target.value
+    })
+  }
+  changeLastName(event){
+    this.setState({
+      LastName:event.target.value
     })
   }
 
-  changelastName(event) {
+  changeEmail(event){
     this.setState({
-      lastName:event.target.value
+      Email:event.target.value
     })
   }
 
-  changeemail(event) {
+  changePassword(event){
     this.setState({
-      email:event.target.value
+      Password:event.target.value
     })
   }
 
-  changepassword(event) {
+  changeConfirmPassword(event){
     this.setState({
-      password:event.target.value
+      ConfirmPassword:event.target.value
     })
   }
-  changeconfirmpassword(event) {
+
+
+  changePhone_Number(event){
     this.setState({
-      confirmpassword:event.target.value
-    })
-  }
-  changenic(event) {
-    this.setState({
-     nic:event.target.value
-      
-    })
-  }
-  changeaddress(event) {
-    this.setState({
-      address:event.target.value
-    })
-  }
-  changecontactNumbers(event) {
-    this.setState({
-      contactNumbers:event.target.value
-    })
-  }
- 
-  changechildName(event) {
-    this.setState({
-      childName:event.target.value
-    })
-  }
-  changechildSchool(event) {
-    this.setState({
-      childSchool:event.target.value
+      Phone_Number:event.target.value
     })
   }
   
-  onSubmit(event) {
-    event.preventDefault()
-
-    const registered = {
-      firstName:this.state.firstName,
-      lastName:this.state.lastName,
-      email:this.state.email,
-      password:this.state.password,
-      confirmpassword:this.state.confirmpassword,
-      nic:this.state.nic,
-      address:this.state.address,
-      contactNumbers:this.state.contactNumbers,
-      childName:this.state.childName,
-      childSchool:this.state.childSchool,
-   
-
-    }
-
-    axios.post('http://localhost:3001/parent/signup', registered)
-      .then(response => console.log(response.data))
-
+  changeNIC_Number(event){
     this.setState({
-      firstName:'',
-      lastName:'',
-      email:'',
-      password:'',
-      confirmpassword:'',
-      nic:'',
-      address:'',
-      contactNumbers:'',
-      childName:'',
-      childSchool:'',
-     
-      
+      NIC_Number:event.target.value
+    })
+  }
+ 
+  changeDriving_Licence_Id(event){
+    this.setState({
+      Driving_Licence_Id:event.target.value
+    })
+  }
+  changeAddress(event){
+    this.setState({
+      Address:event.target.value
     })
   }
 
-  render() {
-    return (
+  onSubmit(event){
+    event.preventDefault()
+
+    const registered={
+        FirstName:this.state.FirstName,
+        LastName:this.state.LastName,
+        Email:this.state.Email,
+        Password:this.state.Password,
+        ConfirmPassword:this.state.ConfirmPassword,
+        Phone_Number:this.state.Phone_Number,
+        NIC_Number:this.state.NIC_Number,
+        Driving_Licence_Id:this.state.Driving_Licence_Id,
+        Address:this.state.Address
       
+    }
 
+    axios.post('http://localhost:3000/Driver/signup', registered)
+        .then(response => console.log(response.data))
 
-        <div className="container">
-          <div className="right"> <img src='https://klfdradio.com/media/2021/09/pexels-photo-4543110.jpeg'></img></div>
+    this.setState({
+        FirstName:'',
+        LastName:'',
+        Email:'',
+        Password:'',
+        ConfirmPassword:'',
+        Phone_Number:'',
+        NIC_Number:'',
+        Driving_Licence_Id:'',
+        Address:''
+      
+    })
+  }
+   
+  render() { 
+    return (
+      <div>
+         <div className="container">
+            <div className="right"> <img src='https://www.clabservice.com/wp-content/uploads/2017/03/driver.jpg'></img></div>
+          
+            <div className="left">
+              <h2 className="signup">Driver Sign Up</h2>
+              <form className="fm" onSubmit={this.onSubmit}>
 
-          <div className="left">
-            <h2 className="signup">School Student Sign Up</h2>
-            <form className="fm" onSubmit={this.onSubmit}>
-              <input type="text"
-                placeholder="Guardian First Name"
-                onChange={this.changefirstName}
-                value={this.state.firstName}
-                className="form-control"
-              />
-              <br />
-              <input type="text"
-                placeholder="Guardian Last Name"
-                onChange={this.changelastName}
-                value={this.state.lastName}
-                className="form-control"
-              />
-              <br />
-              <input type="email"
-                placeholder="E-mail"
-                onChange={this.changeemail}
-                value={this.state.email}
-                className="form-control"
+                <input type="text" 
+                  placeholder="First Name" 
+                  onChange={this.changeFirstName}
+                  value={this.state.FirstName}
+                  className="form-control"
+                />
+                <br/>
 
+                <input type="text" 
+                  placeholder="Last Name" 
+                  onChange={this.changeLastName}
+                  value={this.state.LastName}
+                  className="form-control"
+                />
+                <br/>
+                
+                <input type="email" 
+                  placeholder="E-mail" 
+                  onChange={this.changeEmail}
+                  value={this.state.Email}
+                  className="form-control"
+                />
+                <br/>
+                <input type="password" 
+                  placeholder="Password" 
+                  onChange={this.changePassword}
+                  value={this.state.Password}
+                  className="form-control"
+                />
 
-              />
-              <br />
-              <input type="password"
-                placeholder="Password"
-                onChange={this.changepassword}
-                value={this.state.password}
-                className="form-control"
-              />
-              <br />
-              <input type="password"
-                placeholder="Confirm Password"
-                onChange={this.changeconfirmpassword}
-                value={this.state.confirmpassword}
-                className="form-control"
-              />
-              <br />
-              <input type="text"
-                placeholder="NIC Number"
-                onChange={this.changenic}
-                value={this.state.nic}
-                className="form-control"
-              />
-              <br />
-              <input type="text"
-                placeholder="Address"
-                onChange={this.changeaddress}
-                value={this.state.address}
-                className="form-control"
-              />
-              <br />
-              <input type="text"
-                placeholder="Mobile Number"
-                onChange={this.changecontactNumbers}
-                value={this.state.contactNumbers}  
-                className="form-control"
-              />
-            
-              <h3 className="signup">Children Details</h3>
-             
-              <input type="text"
-                placeholder="Child Name"
-                onChange={this.changechildName}
-                value={this.state.childName}
-                className="form-control"
-              />
-              <br />
-              <input type="text"
-                placeholder="Student School"
-                onChange={this.changechildSchool}
-                value={this.state.childSchool}
-                className="form-control"
-              />
-              <br />
-              <p className="para">If you have two or more children please use separate registrations. </p>
+                <br/>
+                <input type="password"
+                  placeholder="Confirm Password" 
+                  onChange={this.changeConfirmPassword}
+                  value={this.state.ConfirmPassword}
+                  className="form-control"
+                />
 
-              <br />
-              <input type='submit' className="btn" value='Register' />
-            </form>
+                <br/>
+                <input type="text"
+                  placeholder="Phone Number" 
+                  onChange={this.changePhone_Number}
+                  value={this.state.Phone_Number}
+                  className="form-control"
+                />
+                <br/>
+                
+                <input type="text" 
+                  placeholder="NIC Number" 
+                  onChange={this.changeNIC_Number}
+                  value={this.state.NIC_Number}
+                  className="form-control"
+                />
+                <br/>
+                <input type="text" 
+                  placeholder="Driving Licence Id" 
+                  onChange={this.changeDriving_Licence_Id}
+                  value={this.state.Driving_Licence_Id}
+                  className="form-control"
+                />
+                <br/>
+
+                <input type="text" 
+                  placeholder="Address" 
+                  onChange={this.changeAddress}
+                  value={this.state.Address}
+                  className="form-control"
+                />
+                <br/>
+                
+                
+                <input type="submit" className="btn" value='Register'/>
+              </form>
+            </div>
           </div>
-        </div>
-    
+      </div>
     );
   }
 }
-
+ 
 export default App;
