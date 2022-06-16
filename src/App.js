240b085becfe88a1c94f1b15ace71dc43cdf95b1
Autobotbrain <98964236+Autobotbrain@users.noreply.github.com>
2022-06-17 00:54:04 +0530

    validated driver signup

--- src/App.js
+++ src/App.js
@@ -1,209 +1,210 @@
-import React, {Component} from "react";
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state ={
-        FirstName:'',
-        LastName:'',
-        Email:'',
-        Password:'',
-        ConfirmPassword:'',
-        Phone_Number:'',
-        NIC_Number:'',
-        Driving_Licence_Id:'',
-        Address:''
-    }
-
-    this.changeFirstName=this.changeFirstName.bind(this);
-    this.changeLastName=this.changeLastName.bind(this);
-    this.changeEmail=this.changeEmail.bind(this);
-    this.changePassword=this.changePassword.bind(this);
-    this.changeConfirmPassword=this.changeConfirmPassword.bind(this);
-    this.changePhone_Number=this.changePhone_Number.bind(this);
-    this.changeNIC_Number=this.changeNIC_Number.bind(this);
-    this.changeDriving_Licence_Id=this.changeDriving_Licence_Id.bind(this);
-    this.changeAddress=this.changeAddress.bind(this);
-    this.onSubmit=this.onSubmit.bind(this);
-
+  constructor() {
+    super();
+    this.state = {
+      is: false,
+      data: {
+        email: "",
+        fname: "",
+        lname: "",
+        pass: "",
+        cpass: "",
+        pn: "",
+        nic: "",
+        dlid: "",
+        address: "",
+      },
+      errors: {
+        fname: null,
+        lname: null,
+        pass: null,
+        cpass: null,
+        pn: null,
+        nic: null,
+        dlid: null,
+        address: null,
+        errors: null,
+      },
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  changeFirstName(event){
-    this.setState({
-      FirstName:event.target.value
-    })
-  }
-  changeLastName(event){
-    this.setState({
-      LastName:event.target.value
-    })
-  }
+  onSubmit(e) {
+    var is = true;
 
-  changeEmail(event){
-    this.setState({
-      Email:event.target.value
-    })
-  }
-
-  changePassword(event){
-    this.setState({
-      Password:event.target.value
-    })
-  }
-
-  changeConfirmPassword(event){
-    this.setState({
-      ConfirmPassword:event.target.value
-    })
-  }
-
-
-  changePhone_Number(event){
-    this.setState({
-      Phone_Number:event.target.value
-    })
-  }
   
-  changeNIC_Number(event){
-    this.setState({
-      NIC_Number:event.target.value
-    })
-  }
- 
-  changeDriving_Licence_Id(event){
-    this.setState({
-      Driving_Licence_Id:event.target.value
-    })
-  }
-  changeAddress(event){
-    this.setState({
-      Address:event.target.value
-    })
-  }
 
-  onSubmit(event){
-    event.preventDefault()
+    Object.keys(this.state.data).forEach((e) => {
+      this.validation(e, this.state.data[e]);
+    });
+    Object.keys(this.state.errors).forEach((e) => {
+      if (this.state.errors[e] != null) {
+        is = false;
+      }
+    });
 
-    const registered={
-        FirstName:this.state.FirstName,
-        LastName:this.state.LastName,
-        Email:this.state.Email,
-        Password:this.state.Password,
-        ConfirmPassword:this.state.ConfirmPassword,
-        Phone_Number:this.state.Phone_Number,
-        NIC_Number:this.state.NIC_Number,
-        Driving_Licence_Id:this.state.Driving_Licence_Id,
-        Address:this.state.Address
-      
-    }
+    // if (!is) {
+    //   window.alert("check inputs")
+    //   e.preventDefault();
+    //   return false;
+    // }
+    // console.log("sucsess");
 
     axios.post('http://localhost:3000/Driver/signup', registered)
-        .then(response => console.log(response.data))
-
-    this.setState({
-        FirstName:'',
-        LastName:'',
-        Email:'',
-        Password:'',
-        ConfirmPassword:'',
-        Phone_Number:'',
-        NIC_Number:'',
-        Driving_Licence_Id:'',
-        Address:''
-      
-    })
+    .then(response => console.log(response.data))
   }
-   
-  render() { 
+
+  handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ data: { [name]: value } });
+    this.validation(name, value, e.target);
+  }
+
+  validation(name, value) {
+    console.log(name);
+    let errors = this.state.errors;
+    switch (name) {
+      case "fname":
+        console.log("fname is call", value);
+        errors.fname = value.length < 3 ? "first name is requred" : null;
+        break;
+      case "email":
+        errors.email = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)
+          ? null
+          : "Email is Not Valid";
+        break;
+      case "pass":
+        console.log(value);
+        errors.pass =
+          value.length < 8 ? "password must be 8 character long" : null;
+        break;
+      // case "cpass":
+      //   errors.cpass = this.state.pass == this.cpass ? null : "password not mach";
+
+      //   break;
+      case "pn":
+        errors.pn = value.length < 9 ? "Not VAlid" : null;
+        break;
+      case "nic":
+        errors.nic = value.length < 5 ? "Invalid Nic" : null;
+        break;
+      case "dlid":
+        errors.dlid = value.length < 3 ? "driving licen  is requred" : null;
+        break;
+      case "address":
+        errors.address = value.length < 5 ? "address  is requred" : null;
+        break;
+      default:
+    }
+  }
+
+  render() {
+    let errors = this.state.errors;
     return (
       <div>
-         <div className="container">
-            <div className="right"> <img src='https://www.clabservice.com/wp-content/uploads/2017/03/driver.jpg'></img></div>
-          
-            <div className="left">
-              <h2 className="signup">Driver Sign Up</h2>
-              <form className="fm" onSubmit={this.onSubmit}>
-
-                <input type="text" 
-                  placeholder="First Name" 
-                  onChange={this.changeFirstName}
-                  value={this.state.FirstName}
-                  className="form-control"
-                />
-                <br/>
-
-                <input type="text" 
-                  placeholder="Last Name" 
-                  onChange={this.changeLastName}
-                  value={this.state.LastName}
-                  className="form-control"
-                />
-                <br/>
-                
-                <input type="email" 
-                  placeholder="E-mail" 
-                  onChange={this.changeEmail}
-                  value={this.state.Email}
-                  className="form-control"
-                />
-                <br/>
-                <input type="password" 
-                  placeholder="Password" 
-                  onChange={this.changePassword}
-                  value={this.state.Password}
-                  className="form-control"
-                />
-
-                <br/>
-                <input type="password"
-                  placeholder="Confirm Password" 
-                  onChange={this.changeConfirmPassword}
-                  value={this.state.ConfirmPassword}
-                  className="form-control"
-                />
-
-                <br/>
-                <input type="text"
-                  placeholder="Phone Number" 
-                  onChange={this.changePhone_Number}
-                  value={this.state.Phone_Number}
-                  className="form-control"
-                />
-                <br/>
-                
-                <input type="text" 
-                  placeholder="NIC Number" 
-                  onChange={this.changeNIC_Number}
-                  value={this.state.NIC_Number}
-                  className="form-control"
-                />
-                <br/>
-                <input type="text" 
-                  placeholder="Driving Licence Id" 
-                  onChange={this.changeDriving_Licence_Id}
-                  value={this.state.Driving_Licence_Id}
-                  className="form-control"
-                />
-                <br/>
-
-                <input type="text" 
-                  placeholder="Address" 
-                  onChange={this.changeAddress}
-                  value={this.state.Address}
-                  className="form-control"
-                />
-                <br/>
-                
-                
-                <input type="submit" className="btn" value='Register'/>
-              </form>
-            </div>
+        <div className="container">
+          <div className="right">
+            {" "}
+            <img src="https://www.clabservice.com/wp-content/uploads/2017/03/driver.jpg"></img>
           </div>
+
+          <div className="left">
+            <h2 className="signup">Driver Sign Up</h2>
+            <form className="fm" onSubmit={this.onSubmit}>
+              <input
+                type="text"
+                placeholder="First Name"
+                name="fname"
+                onChange={this.handleChange}
+                className="form-control"
+              />
+              <span className="error">{errors.fname}</span>
+              <br />
+              <input
+                type="text"
+                name="lname"
+                placeholder="Last Name"
+                onChange={this.handleChange}
+                className="form-control"
+              />
+              <span className="error">{errors.lname}</span>
+              <br />
+              <input
+                type="email"
+                name="email"
+                onChange={this.handleChange}
+                placeholder="E-mail"
+                className="form-control"
+              />
+              <span className="error">{errors.email}</span>
+              <br />
+              <input
+                name="pass"
+                type="password"
+                onChange={this.handleChange}
+                placeholder="Password"
+                className="form-control"
+              />
+              <span className="error">{errors.pass}</span>
+              <br />
+              <input
+                type="password"
+                name="cpass"
+                placeholder="Confirm Password"
+                onChange={this.handleChange}
+                className="form-control"
+              />
+              <span className="error">{errors.cpass}</span>
+              <br />
+              <input
+                type="text"
+                name="pn"
+                placeholder="Phone Number"
+                onChange={this.handleChange}
+                className="form-control"
+              />
+              <span className="error">{errors.pn}</span>
+              <br />
+              <input
+                type="text"
+                name="nic"
+                placeholder="NIC Number"
+                onChange={this.handleChange}
+                className="form-control"
+              />
+              <span className="error">{errors.nic}</span>
+              <br />
+              <input
+                type="text"
+                name="dlid"
+                placeholder="Driving Licence Id"
+                onChange={this.handleChange}
+                className="form-control"
+              />
+              <span className="error">{errors.dlid}</span>
+              <br />
+              <input
+                type="text"
+                name="address"
+                placeholder="Address"
+                onChange={this.handleChange}
+                className="form-control"
+              />
+              <span className="error">{errors.address}</span>
+              <br />
+              <input type="submit" className="btn" value="Register" />
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
 }
- 
+
 export default App;
